7fb3a39307553e6043162b69b1312871be8bb22d
VG <71305756+VG08@users.noreply.github.com>
2022-08-27 11:37:45 +0530

    remove check

--- database.py
+++ database.py
@@ -11,7 +11,7 @@ SQLALCHEMY_DATABASE_URL = database
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,connect_args={"check_same_thread": False})
+    SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
